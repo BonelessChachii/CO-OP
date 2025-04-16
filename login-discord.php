b116562217ebcae12a79de328d3df17b95e60d16
Charles Montalvo <113223328+BonelessChachii@users.noreply.github.com>
2025-04-15 22:25:14 -0400

    Update login-discord.php
    taken out tokens and url for .env file

--- login-discord.php
+++ login-discord.php
@@ -1,11 +1,11 @@
 <?php
 // Discord OAuth2 login redirect
-$client_id = '1357254475080794184'; // client ID
+$client_id = ''; // client ID
 $redirect_uri = urlencode('http://localhost/CO-OP/discord-callback.php');
 $scope = 'identify email';
 $response_type = 'code';
 
-$auth_url = "https://discord.com/api/oauth2/authorize?client_id=$client_id&redirect_uri=$redirect_uri&response_type=$response_type&scope=$scope";
+$auth_url = "";
 
 header("Location: $auth_url");
 exit();
