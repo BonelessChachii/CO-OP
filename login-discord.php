c70bf27689cce278d6aa03c18fabbf5497ce8957
Charles Montalvo <113223328+BonelessChachii@users.noreply.github.com>
2025-04-15 22:51:09 -0400

    Update login-discord.php
    update to .env file

--- login-discord.php
+++ login-discord.php
@@ -1,11 +1,11 @@
 <?php
 // Discord OAuth2 login redirect
-$client_id = ''; // client ID
+$client_id = ''; //check .env file
 $redirect_uri = urlencode('http://localhost/CO-OP/discord-callback.php');
 $scope = 'identify email';
 $response_type = 'code';
 
-$auth_url = "";
+$auth_url = ""; //check .env file
 
 header("Location: $auth_url");
 exit();
